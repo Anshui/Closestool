6062917d490646535c779146e024e3ce0839024f
zzh <1323218440@qq.com>
2017-10-26 09:38:00 +0800

    设计BaseActivity

--- app/src/main/java/cn/ac/hfcas/closestool/activity/BaseActivity.java
+++ app/src/main/java/cn/ac/hfcas/closestool/activity/BaseActivity.java
@@ -1,15 +1,45 @@
 package cn.ac.hfcas.closestool.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import cn.ac.hfcas.closestool.R;
+import cn.ac.hfcas.closestool.service.BLEService;
 
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class BaseActivity extends AppCompatActivity {
+
+    private final static String TAG = BleSppActivity.class.getSimpleName();
+
+    private BLEService mBluetoothLeService;
+
+    static long recv_cnt = 0;
+
+    private boolean mConnected = false;
+    private String mDeviceAddress;
+
+    private long recvBytes=0;
+    private long lastSecondBytes=0;
+    private long sendBytes;
+    private StringBuilder mData;
+
+    int sendIndex = 0;
+    int sendDataLen=0;
+    byte[] sendBuf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +57,114 @@ public class BaseActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BLEService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+
+        }
+    };
+
+    private void updateConnectionState(final int resourceId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BLEService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BLEService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(BLEService.ACTION_WRITE_SUCCESSFUL);
+        intentFilter.addAction(BLEService.ACTION_GATT_SERVICES_NO_DISCOVERED);
+        return intentFilter;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    private byte[] stringToBytes(String s) {
+        byte[] buf = new byte[s.length() / 2];
+        for (int i = 0; i < buf.length; i++) {
+            try {
+                buf[i] = (byte) Integer.parseInt(s.substring(i * 2, i * 2 + 2), 16);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return buf;
+    }
+
+    public String asciiToString(byte[] bytes) {
+        char[] buf = new char[bytes.length];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < buf.length; i++) {
+            buf[i] = (char) bytes[i];
+            sb.append(buf[i]);
+        }
+        return sb.toString();
+    }
+
+    public String bytesToString(byte[] bytes) {
+        final char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            hexChars[i * 2] = hexArray[v >>> 4];
+            hexChars[i * 2 + 1] = hexArray[v & 0x0F];
+
+            sb.append(hexChars[i * 2]);
+            sb.append(hexChars[i * 2 + 1]);
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
 }
